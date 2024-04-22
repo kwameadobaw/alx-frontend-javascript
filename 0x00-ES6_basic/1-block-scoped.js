5567f79ada243836964c3d92de5d04f8270f6c16
kwameadobaw <kwame.adobawjnr@gmail.com>
2024-04-22 16:47:11 +0000

    task 2 done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  
+    const task = false;
+    const task2 = true;
+
     if (trueOrFalse) {
-      var task = true;
-      var task2 = false;
+        return [task, task2];
     }
-    var task = false;
-    var task2 = true;
+
   
     return [task, task2];
 }
