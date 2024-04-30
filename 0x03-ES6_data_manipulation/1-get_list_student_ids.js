14e4a228a6c9ae5de45b33ce835c239f29f38f6e
kwameadobaw <136028275+kwameadobaw@users.noreply.github.com>
2024-04-30 18:03:30 +0000

    Delete 0x03-ES6_data_manipulation directory

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++
@@ -1,9 +0,0 @@
-export default function getListStudentIds(students) {
-    // Check if the input is an array
-    if (!Array.isArray(students)) {
-        return []; // Return an empty array if input is not an array
-    }
-
-    // Use map function to extract ids from the array of objects
-    return students.map(student => student.id);
-}
