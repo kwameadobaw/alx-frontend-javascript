612f5598f84e01656b2b904c6e6045d28c98875a
kwameadobaw <kwame.adobawjnr@gmail.com>
2024-04-30 17:59:16 +0000

    new changes

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,4 @@
-function getListStudentIds(students) {
+export default function getListStudentIds(students) {
     // Check if the input is an array
     if (!Array.isArray(students)) {
         return []; // Return an empty array if input is not an array
