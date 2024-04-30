9b4878d94ff0047c53170a2f8c3bba94ac70109f
kwameadobaw <kwame.adobawjnr@gmail.com>
2024-04-30 17:54:25 +0000

    First commit

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+function getListStudentIds(students) {
+    // Check if the input is an array
+    if (!Array.isArray(students)) {
+        return []; // Return an empty array if input is not an array
+    }
+
+    // Use map function to extract ids from the array of objects
+    return students.map(student => student.id);
+}
